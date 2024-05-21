d999cf1871b2967844f5f63f1a6278c4418345ca
Damanger <omar.cruzr97@hotmail.com>
2024-05-21 11:50:57 -0600

    Primer avance del landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
         <h1 style={{fontSize:'10rem', fontWeight:'bolder'}}>
           XPT
         </h1>
-        <iframe style={{width:'100vw', height:'100vh', opacity:'0.75', zIndex:'100', top:'0', left:'0', position:'absolute'}} src="https://www.youtube.com/embed/GuLcxg5VGuo?autoplay=1&controls=0&mute=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe style={{width:'100vw', height:'100vh', opacity:'0.75', zIndex:'100', top:'0', left:'0', position:'absolute'}} src="https://www.youtube.com/embed/GuLcxg5VGuo?autoplay=1&controls=0&mute=0&loop=1&playlist=GuLcxg5VGuo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </>
   )
