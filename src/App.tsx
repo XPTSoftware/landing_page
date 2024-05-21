ba83ea00391838d14b46b145ccff320c82e3be7b
Damanger <omar.cruzr97@hotmail.com>
2024-05-21 11:13:00 -0600

    Primer avance del landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <h1 style={{fontSize:'10rem', position:'absolute', left:'50%', top:'50%', transform:'translate(-50%, -50%'}}>
       Leo Puto
-    </div>
+    </h1>
   )
 }
 
