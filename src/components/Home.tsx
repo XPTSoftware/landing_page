273715f49f67bdd70626f896c17bbd9e17d81035
Damanger <omar.cruzr97@hotmail.com>
2024-05-22 01:55:27 -0600

    Añadiendo imagen a XPT

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,14 @@
 import Carousel from './Carousel';
+import '../assets/css/Home.css';
 
 const Home = () => {
     return (
         <>
             <section style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh'}}>
-                <h1 style={{fontSize:'10rem', fontWeight:'bolder'}}>
+                <h1 className='llama-title'>
                     XPT
                 </h1>
-                <iframe style={{width:'100vw', height:'100vh', opacity:'0.75', zIndex:'100', top:'0', left:'0', position:'absolute'}} src="https://www.youtube.com/embed/GuLcxg5VGuo?autoplay=1&controls=0&mute=0&loop=1&playlist=GuLcxg5VGuo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe style={{width:'100vw', height:'100vh', opacity:'0.75', zIndex:'1', top:'0', left:'0', position:'absolute'}} src="https://www.youtube.com/embed/An21fXqyqM0?autoplay=1&controls=0&mute=0&loop=1&playlist=An21fXqyqM0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </section>
             <div>
                 <Carousel />
